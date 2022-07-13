9f73f18958f8ca17fd35a1ff84ae8aa10e9d4e39
mwyzer <muhammad.wyzer@gmail.com>
2022-07-13 10:18:56 +0700

    first commit

---
+++ resources/js/Components/Homepage/Paginator.jsx
@@ -0,0 +1,26 @@
+import { Link } from "@inertiajs/inertia-react";
+
+const Paginator = ({ meta }) => {
+    console.log(meta);
+    const prev = meta.links[0].url;
+    const next = meta.links[meta.links.length - 1].url;
+    const current = meta.current_page;
+
+    return (
+        <div className="btn btn-outline">
+            {prev && (
+                <Link href={prev} className="btn">
+                    «
+                </Link>
+            )}
+            <Link className="btn btn-outline">{current}</Link>
+            {next && (
+                <Link href={next} className="btn btn-outline">
+                    »
+                </Link>
+            )}
+        </div>
+    );
+};
+
+export default Paginator;
